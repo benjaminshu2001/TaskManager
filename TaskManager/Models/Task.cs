796c2faf255bbbfeaa3adbea2834bc23a880c6d8
Benjamin Shu <Benjamin_Shu@kingston.com>
2023-07-07 11:23:39 -0700

    editing Task models

--- TaskManager/Models/Task.cs
+++ TaskManager/Models/Task.cs
@@ -5,6 +5,9 @@
         public int Id { get; set; }
         public string Title { get; set; }
 
-        // Other properties specific to the Task table
+        public string Description { get; set; }
+        public string Category { get; set; }
+        public DateTime DueDate { get; set; }
+        public bool isCompleted { get; set; }
     }
 }
---
+++ TaskManager/Models/TaskManagerDbContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace TaskManager.Models
+{
+    public class TaskManagerDbContext : IdentityDbContext
+    {
+        public TaskManagerDbContext(DbContextOptions<TaskManagerDbContext> options) : base(options) { }
+
+    }
+}
