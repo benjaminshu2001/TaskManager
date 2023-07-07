49ac5eb1888dfcf0ee7b34159f1f5b1560a84e58
bshu <bshu@BSHU-MD>
2023-07-07 11:05:28 -0700

    Add project files.

---
+++ TaskManager/Models/ITask.cs
@@ -0,0 +1,16 @@
+﻿namespace TaskManager.Models
+{
+    public interface ITask
+    {
+        int Id { get; set; }
+        string Title { get; set; }
+        string Description { get; set; }
+        string Category{ get; set; }
+        DateTime DueDate { get; set; }
+        bool isCompleted { get; set; }
+
+    }
+
+
+
+}
---
+++ TaskManager/Models/Task.cs
@@ -0,0 +1,10 @@
+﻿namespace TaskManager.Models
+{
+    public class Task : ITask
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+
+        // Other properties specific to the Task table
+    }
+}
